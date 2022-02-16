ee9b9ca0fe5a85b8e623e5723dfeab728090a26e
sammydev <sbee1796@gmail.com>
2022-02-16 23:45:13 +0100

    Added a spinner component for loading users

---
+++ src/components/layouts/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
+
+const Spinner = () => {
+  return (
+    <Oval  heigth="100"
+    width="100"
+    color='green'
+    ariaLabel='loading' />
+  )
+}
+
+export default Spinner;
