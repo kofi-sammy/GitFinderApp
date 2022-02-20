c81e2527be006dd65b7a6e1833397ef128a7a937
sammydev <sbee1796@gmail.com>
2022-02-20 23:39:16 +0100

    updated the UserResults Component with a Context for managing users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,46 +1,23 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
+import Spinner from "../layouts/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
-const api = axios.create({
-  baseURL: "https://api.github.com",
-});
 
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [isloading, setIsLoading] = useState(false);
-  const [error, setError] = useState('')
+  const {users, loading, error}  = useContext(GithubContext)
 
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  const getUser = async () => {
-    try {
-      setError(null)
-      setIsLoading(true);
-      const res = await api.get(`/users`, {
-        headers: {
-          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      });
-
-      setUsers(res.data);
-      setIsLoading(false);
-    } catch (err) {
-      setIsLoading(false);
-      setError(err.messsage);
-    }
-  };
-
-  if (isloading) return <h1>Loading....</h1>;
-  if (error) return <h1>{error}</h1>
+  if (loading) return <div className="h-screen flex flex-col items-center justify-center">
+     <Spinner />
+  </div> 
+  if (error) return <h1>{error}</h1>;
   return (
     <div
       className="grid grid-cols-1 gap-8 xl:grid-cols-4 
         lg:grid-cols-3 md:grid-cols-2"
     >
       {users.map((user) => (
-        <h3 key={user.id}>{user.login}</h3>
+        <UserItem key={user.id} user={user}/>
       ))}
     </div>
   );
