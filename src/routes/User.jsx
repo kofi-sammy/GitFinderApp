9f1fb5736eb2ebb330ce6f817b81e470fd77419e
sammydev <sbee1796@gmail.com>
2022-03-15 06:16:32 +0100

    Added a User Component to display user info

---
+++ src/routes/User.jsx
@@ -0,0 +1,82 @@
+import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
+import  {useContext, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Spinner from '../components/layouts/Spinner';
+import GithubContext from '../context/github/GithubContext';
+
+const User = () => {
+  const params = useParams()
+  const {getUser,user,loading} = useContext(GithubContext)
+  
+  useEffect(() => {
+     getUser(params.login)
+   }, [])
+
+  const {
+    name,
+    type, 
+    avatar_url,
+    location,bio, 
+    blog, 
+    twitter_username,
+    login,html_url,
+    followers, 
+    following, 
+    public_repos, 
+    public_gist,
+    hireable
+  } = user
+
+  console.log(user)
+
+   if(loading) return <div className="h-screen flex flex-col items-center justify-center">
+       <Spinner/>
+     </div>
+   
+  return <>
+     <div className="w-full mx-auto lg:w-10/12">
+       <div className="mb-4">
+         <Link to="/" className='btn btn-ghost'>
+           Back to Search
+         </Link>
+       </div>
+       <div className="grid grid-cols-1 xl:grid-cols-3 
+           lg:grid-cols-3 md:gird-cols-3 mb-8 md:gap-8">
+           <div className="custom-card-image mb-6 md:mb-0">
+             <div className="rounded-lg shadow-xl card image-full">
+               <figure>
+                 <img src={avatar_url} alt='github-image'/>
+               </figure>
+               <div className="card-body justify-end">
+                 <h2 className='card-title mb-0'>{name}</h2>
+                 <p>{login}</p>
+               </div>
+             </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className='text-3xl card-title'>
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">
+                  {type}
+                </div>
+                {hireable && (
+                  <div className="badge badge-info">
+                    Hireable
+                  </div>
+                )}
+              </h1>
+              <p>{bio}</p>
+               <a href={html_url} target='_blank' rel='noreferrer' className='btn btn-outline m-4'>
+                 Visit Profile
+               </a>
+            </div>
+          </div>
+       </div>
+     </div>
+    </>
+  
+}
+
+export default User;
